60ed64a6e7350f407ee0b9f5511f450070fc90f9
henry-pulver <henrypulver@hotmail.co.uk>
2023-06-29 17:19:48 +0100

    Make Superflows not require sign-in locally

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import { Navbar } from "../components/navbar";
 import Playground from "../components/playground";
-import { useSession } from "@supabase/auth-helpers-react";
-import React from "react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import React, { useEffect } from "react";
 import SignInComponent from "../components/signIn";
 import { useProfile } from "../components/contextManagers/profile";
 import CreateOrgScreen from "../components/onboarding/createOrg";
 import Headers from "../components/headers";
+import { LoadingSpinner } from "../components/loadingspinner";
 
 export default function App() {
   return (
@@ -19,10 +20,37 @@ export default function App() {
 
 function Dashboard() {
   const session = useSession();
-  const { profile } = useProfile();
+  const { profile, refreshProfile } = useProfile();
+  const supabase = useSupabaseClient();
+  const isDev = process.env.NODE_ENV === "development";
+
+  useEffect(() => {
+    (async () => {
+      if (isDev) {
+        const res = await supabase.auth.signInWithPassword({
+          email: "localuser@gmail.com",
+          password: "password",
+        });
+        if (!res.data.session) {
+          const res2 = await supabase.auth.signUp({
+            email: "localuser@gmail.com",
+            password: "password",
+            options: { data: { full_name: "Local User" } },
+          });
+        }
+        if (refreshProfile) refreshProfile();
+      }
+    })();
+  }, [refreshProfile, supabase]);
 
   return !session ? (
-    <SignInComponent />
+    !isDev ? (
+      <SignInComponent />
+    ) : (
+      <div className="bg-gray-800 h-screen w-screen flex justify-center place-items-center">
+        <LoadingSpinner classes={"h-20 w-20 text-gray-300"} />
+      </div>
+    )
   ) : profile?.org_id === null ? (
     <CreateOrgScreen />
   ) : (
