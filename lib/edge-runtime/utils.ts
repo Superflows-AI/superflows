08a5ed79f9a5ef7348833f28b9251bf441ba1767
henry-pulver <henrypulver@hotmail.co.uk>
2023-06-29 14:00:19 +0100

    Everything behind the API

--- lib/edge-runtime/utils.ts
+++ lib/edge-runtime/utils.ts
@@ -1,11 +1,10 @@
 import { NextRequest } from "next/server";
-import { Database } from "../database.types";
-import { ActionGroupJoinActions, ConversationsJoinMessages } from "../types";
+import { ActionGroupJoinActions, ChatMessage, Organization } from "../types";
 import { ChatGPTMessage } from "../models";
 
 export async function getOrgFromToken(
   req: NextRequest
-): Promise<Database["public"]["Tables"]["organizations"]["Row"] | undefined> {
+): Promise<Organization | undefined> {
   let token = req.headers
     .get("Authorization")
     ?.replace("Bearer ", "")
@@ -27,18 +26,6 @@ export async function getOrgFromToken(
   }
   return undefined;
 }
-
-// async function hashOnEdgeRuntime(message: string): Promise<string> {
-//   const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
-//   const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
-//   const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
-//   return (
-//     hashArray
-//       // .map((b) => b.toString(16).padStart(1, "0"))
-//       .map((b) => b.toString(16))
-//       .join("")
-//   ); // convert bytes to hex string
-// }
 
 export async function getActiveActionGroupsAndActions(
   orgId: number
@@ -63,7 +50,7 @@ export async function getConversation(
 ): Promise<ChatGPTMessage[] | undefined> {
   // Below gets the action groups and actions that are active
   let authRequestResult = await fetch(
-    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/conversations?select=*%2Cchat_messages%28*%29&conversations.id=eq.${conversationId}`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/chat_messages?select=*&conversation_id=eq.${conversationId}`,
     {
       headers: {
         Authorization: `Bearer ${process.env.SERVICE_LEVEL_KEY_SUPABASE}`,
@@ -73,8 +60,10 @@ export async function getConversation(
   );
   const jsonResponse = await authRequestResult.json();
   if (jsonResponse.error) throw new Error(jsonResponse.error.message);
+  console.log("jsonResponse", jsonResponse);
   if (jsonResponse && jsonResponse.length > 0) {
-    (jsonResponse as ConversationsJoinMessages).chat_messages
+    // @ts-ignore
+    return (jsonResponse as ChatMessage[])
       .sort((m1, m2) => m1.conversation_index - m2.conversation_index)
       .map((m) =>
         m.role !== "function"
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -6,8 +6,10 @@ import Headers from "../components/headers";
 import { useProfile } from "../components/contextManagers/profile";
 import {
   ArrowPathIcon,
+  CheckCircleIcon,
   EyeIcon,
   EyeSlashIcon,
+  XCircleIcon,
 } from "@heroicons/react/24/outline";
 import classNames from "classnames";
 import { generateApiKey } from "../lib/apiKey";
@@ -27,20 +29,25 @@ function Dashboard() {
   const { profile } = useProfile();
   const supabase = useSupabaseClient();
   const [token, setToken] = React.useState<string>("");
+  const [showKey, setShowKey] = React.useState<boolean>(false);
   const [copyFeedback, setCopyFeedback] = React.useState<boolean>(false);
   const [warningOpen, setWarningOpen] = React.useState<boolean>(false);
-  const [previousKey, setPreviousKey] = React.useState<boolean>(false);
+  const [apiHost, setApiHost] = React.useState<string>("https://");
+  const [apiHostSavedFeedback, setApiHostSavedFeedback] = React.useState<
+    boolean | null
+  >(null);
 
   useEffect(() => {
     (async () => {
       if (profile) {
         const res = await supabase
           .from("organizations")
-          .select("api_key")
+          .select("api_key, api_host")
           .eq("id", profile?.org_id);
         if (res.error) throw res.error;
         if (res.data[0]) {
-          setPreviousKey(true);
+          setToken(res.data[0].api_key);
+          setApiHost(res.data[0].api_host);
         }
       }
     })();
@@ -80,30 +87,35 @@ function Dashboard() {
         <Navbar current={"API"} />
         <div className="h-[calc(100vh-4rem)] flex flex-col gap-y-4 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mt-12 bg-gray-850 rounded-md px-6 py-4">
-            <div className="">
-              <h2 className="text-lg text-gray-200">Project API key</h2>
-              <p className="mt-1 text-gray-400">
-                Your API is secured behind an API gateway which requires an API
-                Key for every request.
-                <br />
-                This is a secret and should not be shared with anyone.
-              </p>
-            </div>
+            <h2 className="text-lg text-gray-200">Project API key</h2>
+            <p className="mt-1 text-gray-400">
+              The Superflows API is secured behind a gateway which requires an
+              API Key for every request.
+              <br />
+              This is a secret and should not be shared with anyone.
+            </p>
             <div className="flex flex-col place-items-end">
               <div className="flex flex-row gap-x-3 mt-4 justify-end">
-                {(previousKey || token) && (
+                {token && (
                   <div className="font-mono rounded font-bold bg-red-700 text-gray-100 text-sm px-1.5 py-1 h-fit">
                     secret
                   </div>
                 )}
                 <div className="relative border border-gray-600 rounded-md bg-gray-800 text-gray-400 px-20 py-0.5 text-center">
-                  {token || "sfk-********-****-****-****-************"}
-                  <div className="absolute left-2 top-1">
-                    {!token ? (
-                      <EyeSlashIcon className="h-5 w-5 text-gray-500" />
-                    ) : (
-                      <EyeIcon className="h-5 w-5 text-gray-500" />
-                    )}
+                  {showKey ? token : "sfk-********-****-****-****-************"}
+                  <div className="absolute left-1.5 top-0.5">
+                    <button
+                      className={
+                        "rounded-md px-0.5 py-0.5 text-gray-300 bg-gray-800 hover:bg-gray-700"
+                      }
+                      onClick={() => setShowKey(!showKey)}
+                    >
+                      {!showKey ? (
+                        <EyeSlashIcon className="h-5 w-5 text-gray-500" />
+                      ) : (
+                        <EyeIcon className="h-5 w-5 text-gray-500" />
+                      )}
+                    </button>
                   </div>
                   {token && (
                     <button
@@ -119,38 +131,65 @@ function Dashboard() {
                 </div>
                 <button
                   onClick={async () => {
-                    if (previousKey) setWarningOpen(true);
-                    else {
-                      // If no previous key, just generate a new one
-                      const key = generateApiKey();
-                      setToken(key);
-                      const res = await supabase
-                        .from("organizations")
-                        .update({ api_key: key })
-                        .eq("id", profile?.org_id);
-                      if (res.error) throw res.error;
-                    }
+                    setWarningOpen(true);
                   }}
                   className={classNames(
-                    "border border-gray-600 rounded-md px-2 py-0.5 text-gray-300 flex flex-row gap-x-1 place-items-center",
-                    previousKey || token
-                      ? "bg-gray-800"
-                      : "bg-purple-800 hover:bg-purple-700"
+                    "border border-gray-600 rounded-md px-2 py-0.5 text-gray-300 flex flex-row gap-x-1 place-items-center bg-gray-800"
                   )}
                 >
                   <ArrowPathIcon className="h-5 w-5" />
-                  {previousKey ? "Regenerate key" : "Generate key"}
+                  Regenerate key
                 </button>
               </div>
-              <p
-                className={classNames(
-                  token ? "text-gray-400" : "text-transparent",
-                  "mt-3"
-                )}
-              >
-                Copy this key now,{" "}
-                <b>you won&apos;t be able to access it again</b>.
-              </p>
+            </div>
+          </div>
+
+          <div className="mt-6 bg-gray-850 rounded-md px-6 py-4">
+            <h2 className="text-lg text-gray-200">Your API host</h2>
+            <p className="mt-1 text-gray-400">
+              This is the url where your API is hosted.
+              <br />
+              While Superflows is in beta, we recommend you set this to a test
+              value.
+            </p>
+            <div className="flex flex-row gap-x-3 mt-6 mb-3 justify-end">
+              <div className="relative flex flex-col w-[calc(50%+8rem)]">
+                <div
+                  className={classNames(
+                    "absolute left-0 top-2 flex flex-row place-items-center gap-x-1 text-gray-200",
+                    apiHostSavedFeedback !== null ? "" : "invisible"
+                  )}
+                >
+                  {apiHostSavedFeedback ? (
+                    <CheckCircleIcon className="h-5 w-5 text-green-500" />
+                  ) : (
+                    <XCircleIcon className="h-5 w-5 text-red-500" />
+                  )}
+                  {apiHostSavedFeedback ? "Saved" : "Save failed"}
+                </div>
+                <input
+                  className="ml-32 border border-gray-600 rounded-md bg-gray-800 text-gray-200 px-5 w-[calc(100%-8rem)] py-1"
+                  onChange={(e) => setApiHost(e.target.value)}
+                  value={apiHost}
+                  onBlur={async () => {
+                    const res = await supabase
+                      .from("organizations")
+                      .update({ api_host: apiHost })
+                      .eq("id", profile?.org_id);
+                    if (res.error) throw res.error;
+                    setApiHostSavedFeedback(!res.error);
+                    setTimeout(() => {
+                      setApiHostSavedFeedback(null);
+                    }, 2000);
+                  }}
+                />
+                <p className="w-full mt-3 flex justify-center place-items-baseline text-gray-500">
+                  This should probably start with{" "}
+                  <code className="ml-1 rounded-md bg-gray-800 px-1 text-gray-500">
+                    https://
+                  </code>
+                </p>
+              </div>
             </div>
           </div>
         </div>
