01a1ab4e4f43443d384522bbfa5e0cb9462eef32
James Rowland <jrowlandm@gmail.com>
2023-07-21 14:17:51 +0100

    added delete all actions button (#31)
    * added delete all actions button
    
    * disable clear all button when there are no actions
    
    * restyle button

--- components/actions/actionsSection.tsx
+++ components/actions/actionsSection.tsx
@@ -2,6 +2,7 @@ import {
   DocumentArrowUpIcon,
   EllipsisHorizontalIcon,
   PlusIcon,
+  TrashIcon,
 } from "@heroicons/react/24/outline";
 import {
   SupabaseClient,
@@ -32,6 +33,8 @@ export default function PageActionsSection(props: {
   const [open, setUploadModalOpen] = useState<boolean>(false);
   const [includeInactive, setIncludeInactive] = useState<boolean>(true);
   const [numActiveActions, setNumActiveActions] = useState<number>(0);
+  const [deleteAllActionsModelOpen, setDeleteAllActionsModalOpen] =
+    useState<boolean>(false);
 
   useEffect(() => {
     setNumActiveActions(
@@ -61,6 +64,23 @@ export default function PageActionsSection(props: {
         setOpen={setUploadModalOpen}
         loadActions={props.loadActions}
       />
+      <WarningModal
+        title={`Delete all actions?`}
+        description={
+          "Are you sure you want to delete all actions? Once you delete them you can't get it back. There's no undo button."
+        }
+        action={async () => {
+          props.setActionTags([]);
+          const res = await supabase
+            .from("action_tags")
+            .delete()
+            .match({ org_id: profile?.org_id });
+          if (res.error) throw res.error;
+        }}
+        open={deleteAllActionsModelOpen}
+        setOpen={setDeleteAllActionsModalOpen}
+      />
+
       <div className="mt-5 mx-5 mb-20">
         <div className="flex w-full place-items-end justify-between gap-x-2">
           <div className="flex flex-row gap-x-6 place-items-center">
@@ -106,8 +126,8 @@ export default function PageActionsSection(props: {
                   props.setActionTags(newActionTags);
                 }}
               >
-                <PlusIcon className="text-gray-200 w-4 h-4 md:w-5 md:h-5" /> Add
-                tag
+                <PlusIcon className="text-gray-200 w-4 h-4 md:w-5 md:h-5" />
+                Add tag
               </button>
             )}
             <button
@@ -116,6 +136,21 @@ export default function PageActionsSection(props: {
             >
               <DocumentArrowUpIcon className="text-gray-200 w-4 h-4 md:w-5 md:h-5" />{" "}
               Upload
+            </button>
+            <button
+              className={classNames(
+                "flex flex-row place-items-center gap-x-1 bg-gray-900 text-red-700 font-medium text-xs md:text-sm py-1.5 px-4 rounded  ",
+                props.actionTags.length > 0
+                  ? "hover:text-white hover:bg-red-700 focus:ring-2"
+                  : "cursor-not-allowed opacity-50"
+              )}
+              onClick={() =>
+                props.actionTags.length > 0 &&
+                setDeleteAllActionsModalOpen(true)
+              }
+            >
+              <TrashIcon className=" w-4 h-4 md:w-5 md:h-5" />
+              Delete all
             </button>
           </div>
         </div>
