141264dd23b0c15415476649f8f5d934e8c40486
henry-pulver <henrypulver@hotmail.co.uk>
2023-07-24 15:37:21 +0100

    Make playground error message fit situation & add tests
    The issue was with extracting matches when there were
    newlines in the commands. This led to the thoughts and
    tell user showing multiple times in one message.

---
+++ __tests__/splitContentByParts.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, it } from "@jest/globals";
+import { splitContentByParts } from "../components/playgroundChatbot";
+
+describe("splitContentByParts", () => {
+  it("Does nothing to simple string", () => {
+    const matches = splitContentByParts("Hello world");
+    expect(matches).toEqual(["Hello world"]);
+  });
+  it("Splits on <table>", () => {
+    const matches = splitContentByParts("Hello <table>world</table>");
+    expect(matches).toEqual(["Hello ", "<table>world</table>"]);
+  });
+  it("Splits on <button>", () => {
+    const matches = splitContentByParts("Hello <button>world</button>");
+    expect(matches).toEqual(["Hello ", "<button>world</button>"]);
+  });
+  it("Multiple <button>s & <table>s", () => {
+    const matches = splitContentByParts(
+      "Hello <button>world</button>\n\n<table>You're looking</table>\n\n<table>good today</table>"
+    );
+    expect(matches).toStrictEqual([
+      "Hello ",
+      "<button>world</button>",
+      "\n\n",
+      "<table>You're looking</table>",
+      "\n\n",
+      "<table>good today</table>",
+    ]);
+  });
+});
