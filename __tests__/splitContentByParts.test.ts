ed880c83f82f53413a3d9596c03f577cea9d66c3
henry-pulver <henrypulver@hotmail.co.uk>
2023-08-06 15:48:49 +0100

    README tweaks

--- __tests__/splitContentByParts.test.ts
+++ __tests__/splitContentByParts.test.ts
@@ -6,25 +6,21 @@ describe("splitContentByParts", () => {
     const matches = splitContentByParts("Hello world");
     expect(matches).toEqual(["Hello world"]);
   });
-  it("Splits on <table>", () => {
-    const matches = splitContentByParts("Hello <table>world</table>");
-    expect(matches).toEqual(["Hello ", "<table>world</table>"]);
-  });
   it("Splits on <button>", () => {
     const matches = splitContentByParts("Hello <button>world</button>");
     expect(matches).toEqual(["Hello ", "<button>world</button>"]);
   });
-  it("Multiple <button>s & <table>s", () => {
+  it("Multiple <button>s", () => {
     const matches = splitContentByParts(
-      "Hello <button>world</button>\n\n<table>You're looking</table>\n\n<table>good today</table>"
+      "Hello <button>world</button>\n\n<button>You're looking</button>\n\n<button>good today</button>"
     );
     expect(matches).toStrictEqual([
       "Hello ",
       "<button>world</button>",
       "\n\n",
-      "<table>You're looking</table>",
+      "<button>You're looking</button>",
       "\n\n",
-      "<table>good today</table>",
+      "<button>good today</button>",
     ]);
   });
 });
