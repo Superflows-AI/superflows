32929f57cee1023f35b4e98da1b92fc4667b9fe2
henry-pulver <henrypulver@hotmail.co.uk>
2023-08-07 15:24:57 +0100

    Deal with invalid json in db

--- lib/edge-runtime/utils.ts
+++ lib/edge-runtime/utils.ts
@@ -20,14 +20,23 @@ export async function getActiveActionTagsAndActions(
 }
 
 export function DBChatMessageToGPT(message: DBChatMessage): ChatGPTMessage {
-  if (message.role === "function")
-    return {
-      role: message.role,
+  if (message.role === "function") {
+    let content: string;
+    try {
       // Below conversion to JSON and back to string remove all newlines, indentation etc
       // which are empty tokens. This can cut tokens by ~half
-      content: JSON.stringify(JSON.parse(message.content)),
+      content = JSON.stringify(JSON.parse(message.content));
+    } catch (e) {
+      // If invalid JSON
+      content = message.content;
+    }
+
+    return {
+      role: message.role,
+      content: content,
       name: message.name!,
     };
+  }
   return {
     role: message.role as "user" | "assistant",
     content: message.content,
