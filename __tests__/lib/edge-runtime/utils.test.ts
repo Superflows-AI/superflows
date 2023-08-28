ff4bbd1bc727818a223af37b9df3163f07c648a9
henry-pulver <henrypulver@hotmail.co.uk>
2023-08-28 10:49:37 +0100

    Remove summary from GPT message and bump chat-ui

---
+++ __tests__/lib/edge-runtime/utils.test.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect } from "@jest/globals";
+import { MessageInclSummaryToGPT } from "../../../lib/edge-runtime/utils";
+
+describe("MessageInclSummaryToGPT", () => {
+  it("user", () => {
+    expect(
+      MessageInclSummaryToGPT({
+        role: "user",
+        content: "hello",
+      })
+    ).toEqual({
+      role: "user",
+      content: "hello",
+    });
+  });
+  it("function, no summary", () => {
+    expect(
+      MessageInclSummaryToGPT({
+        role: "function",
+        name: "test",
+        content: "hello",
+      })
+    ).toEqual({
+      role: "function",
+      name: "test",
+      content: "hello",
+    });
+  });
+  it("function, summary", () => {
+    expect(
+      MessageInclSummaryToGPT({
+        role: "function",
+        name: "test",
+        content:
+          "very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long string",
+        summary: "long string",
+      })
+    ).toEqual({
+      role: "function",
+      name: "test",
+      content: "long string",
+    });
+  });
+});
