26b399f67492b6a96814469960948ca05e4f4a15
neuromantic99 <jrowlandm@gmail.com>
2023-10-10 11:06:30 +0100

    add function to query openai embedding endpoint

--- lib/models.ts
+++ lib/models.ts
@@ -110,3 +110,13 @@ export interface Properties {
     data?: any;
   };
 }
+
+export interface EmbeddingResponse {
+  data: { embedding: number[]; index: number; object: string }[];
+  model: string;
+  object: string;
+  usage: {
+    prompt_tokens: number;
+    total_tokens: number;
+  };
+}
