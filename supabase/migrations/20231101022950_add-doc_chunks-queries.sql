1f0330c9aaec1c9741cf26473121a26d975ba765
Istvan Kreisz <kreiszdev@gmail.com>
2023-10-31 23:00:40 -0500

    fix get_all_page_section_counts

--- supabase/migrations/20231101022950_add-doc_chunks-queries.sql
+++ supabase/migrations/20231101022950_add-doc_chunks-queries.sql
@@ -17,14 +17,13 @@ end;
 $$;
 
 create or replace function get_all_page_section_counts()
-return int (
+returns integer (
 ) language plpgsql as $$
 begin
-    return query
-    select count(*) from
+    return (select count(*) from
     (select page_url, section_title, count(*)
     from doc_chunks
     group by page_url, section_title
-    order by page_url, section_title) as sections;
+    order by page_url, section_title) as sections);
 end;
 $$;
