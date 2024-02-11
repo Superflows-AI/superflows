9d14401d32f8861f7ba17a8a1ea3f772a049f23d
henry-pulver <henrypulver@hotmail.co.uk>
2024-02-11 22:25:25 +0000

    Add code execution endpoint

---
+++ supabase/functions/execute-code-2/actionUtils.ts
@@ -0,0 +1,118 @@
+import { OpenAPIV3_1 } from "openapi-types";
+
+export function isChoiceRequired(
+  schema: OpenAPIV3_1.SchemaObject,
+  parentRequired: boolean = true,
+): boolean {
+  /** Determines whether there's a choice to be made by the AI for
+   * a parameter. There's no choice if it is required and has 1 enum
+   * value (or all nested children are like this) **/
+  if (!parentRequired) return true;
+  if (schema.type !== "object") {
+    return !(schema.enum && schema.enum.length === 1);
+  }
+
+  const requiredKeys = schema.required || [];
+
+  for (let key in schema.properties) {
+    const isKeyRequired = requiredKeys.includes(key);
+    const childSchema = schema.properties[key] as OpenAPIV3_1.SchemaObject;
+
+    // If any nested object requires choice, then choice is required.
+    if (isChoiceRequired(childSchema, isKeyRequired)) {
+      return true;
+    }
+  }
+
+  // If no choice required in any properties, then no choice required.
+  return false;
+}
+
+export function getFilledNoChoiceRequiredFields(
+  schema: OpenAPIV3_1.SchemaObject | undefined,
+  parentRequired: boolean = true,
+): any {
+  /** Gets an object with filled fields where no choice is required by the AI.
+   *
+   * Used after the AI call, before API call, to get the parameters
+   * where no choice is required, but the parameters need to be filled
+   * for the API call to succeed. **/
+  // TODO: Doesn't deal with arrays yet, only deals with nested objects
+  if (!schema || schema.type !== "object" || !parentRequired) {
+    return null;
+  }
+  // Below is for case where it's an object schema but that's all you know
+  if (Object.keys(schema).length === 1) {
+    return {};
+  }
+
+  let filledObject: any = {};
+  const requiredKeys = schema.required || [];
+
+  for (let key in schema.properties) {
+    const isKeyRequired = requiredKeys.includes(key);
+    const childSchema = schema.properties[key] as OpenAPIV3_1.SchemaObject;
+
+    // If key is required and has only one enum value, add it into the filled object.
+    if (isKeyRequired && childSchema.enum && childSchema.enum.length === 1) {
+      filledObject[key] = childSchema.enum[0];
+    } else if (childSchema.type === "object") {
+      // Else if the property is an object, then recursively fill its parameters.
+      const filledChildObject = getFilledNoChoiceRequiredFields(
+        childSchema,
+        isKeyRequired,
+      );
+      if (filledChildObject) {
+        filledObject[key] = filledChildObject;
+      }
+    }
+  }
+
+  return Object.keys(filledObject).length > 0 ? filledObject : null;
+}
+
+export function fillNoChoiceRequiredParams<
+  Params extends Record<string, unknown>,
+  Output extends Params,
+>(paramsFromAI: Params, schema: OpenAPIV3_1.SchemaObject): Output {
+  /** Combines the parameters from the AI with parameters where no
+   * choice is required by the AI. **/
+  const noChoiceParams = getFilledNoChoiceRequiredFields(schema);
+  return combineParams(paramsFromAI, noChoiceParams);
+}
+
+interface Params {
+  [key: string | number]: any;
+}
+
+function combineParams<
+  ParamsA extends Params,
+  ParamsB extends Params,
+  OutParams extends ParamsA & ParamsB,
+>(a: ParamsA, b: ParamsB): OutParams {
+  /** This function combines two objects, with b overwriting a in
+   * the case of a clash. **/
+  // TODO: Doesn't deal with arrays yet, only deals with nested objects
+  let out = { ...a } as unknown as OutParams;
+
+  for (let key in b) {
+    if (b.hasOwnProperty(key)) {
+      // If key is in both objects and both values are objects,
+      //  then recursively combine them
+      if (
+        typeof b[key] === "object" &&
+        b[key] !== null &&
+        !Array.isArray(b[key]) &&
+        out[key] &&
+        typeof out[key] === "object" &&
+        !Array.isArray(out[key])
+      ) {
+        out[key] = combineParams(out[key], b[key]);
+      } else {
+        // @ts-ignore
+        out[key] = b[key];
+      }
+    }
+  }
+  return out;
+}
---
+++ supabase/functions/execute-code-2/utils.ts
@@ -0,0 +1,189 @@
+export async function exponentialRetryWrapper<Args extends Array<any>, Output>(
+  func: (...args: Args) => Promise<Output>,
+  args: Args,
+  retries: number,
+): Promise<Output> {
+  const t1 = Date.now();
+  console.warn("Starting exponentialRetryWrapper for function " + func.name);
+  try {
+    const res = await func(...args);
+    console.warn(
+      `Exponential retry wrapper completed in ${Date.now() - t1} ms for func "${
+        func.name
+      }". There were ${retries - 1} retries remaining.`,
+    );
+    return res;
+  } catch (error) {
+    console.warn(
+      `Error in exponentialRetryWrapper for function ${func.name}. The error is: ${error}}`,
+    );
+    if (retries > 0) {
+      console.warn(`Retrying ${func.name} in ${2 ** (10 - retries)}ms`);
+      await new Promise((r) => setTimeout(r, 2 ** (10 - retries)));
+      return await exponentialRetryWrapper(func, args, retries - 1);
+    } else {
+      throw error;
+    }
+  }
+}
+
+function deleteUndefined<InputType extends Record<string, any | undefined>>(
+  obj: InputType,
+): Partial<InputType> {
+  for (let key in obj) {
+    if (obj[key] === undefined) {
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
+export function deduplicateArray<ArrType extends any[]>(
+  arr: ArrType,
+): Partial<ArrType | { items: Partial<ArrType>[] }> {
+  if (arr.length === 0) return {};
+  // Check for null, undefined, string, number, boolean or array
+  if (
+    arr.some((ele) => !ele || typeof ele !== "object" || Array.isArray(ele))
+  ) {
+    return arr;
+  }
+  const firstEle = arr[0];
+  let output: Record<string, any> = { items: [] };
+
+  Object.keys(firstEle).forEach((key) => {
+    const allValues = arr.map((ele) => ele[key]);
+    if (allValues.every((val) => val === allValues[0])) {
+      output[key] = allValues[0];
+    } else if (output.items.length === 0) {
+      output.items = arr.map((ele) => ({ [key]: ele[key] }));
+    } else {
+      output.items.forEach((item: any, i: number) => {
+        item[key] = arr[i][key];
+      });
+    }
+  });
+  if (output.items.length === 0) {
+    delete output.items;
+  } else {
+    output.items = output.items.map((item: any) => {
+      return deleteUndefined(item);
+    });
+  }
+
+  // If there's no duplication (everything in 'items') then keep it as an array
+  if (Object.keys(output).length === 1 && "items" in output) {
+    return output.items;
+  }
+
+  return output;
+}
+
+export function filterKeys<InputObject extends any>(
+  obj: InputObject,
+  keysToKeep: string[],
+): any {
+  if (!obj || typeof obj !== "object") return obj;
+  else if (Array.isArray(obj)) {
+    return obj.map((ele) => filterKeys(ele, keysToKeep));
+  } else {
+    const output: Record<string, any> = {};
+    Object.entries(obj).forEach(([key, value]) => {
+      // Not array or object
+      if (!value || typeof value !== "object") {
+        if (keysToKeep.includes(key)) {
+          output[key] = value;
+        }
+        return;
+      }
+      // Array
+      if (Array.isArray(value)) {
+        // If the array is empty, don't add it
+        const val = value.map((ele) => filterKeys(ele, keysToKeep));
+        if (val.length > 0) {
+          output[key] = val;
+        }
+      }
+      // Object
+      const val = filterKeys(value, keysToKeep);
+      // If the object is empty, don't add it
+      if (Object.keys(val).length > 0) {
+        output[key] = val;
+      }
+    });
+
+    return output;
+  }
+}
+
+export function snakeToCamel(str: string): string {
+  return str.replace(/(_\w)/g, function (m: string): string {
+    return m[1].toUpperCase();
+  });
+}
+
+export function getJsonMIMEType<inObj>(
+  inputDict: Record<string, inObj> | undefined | null,
+): inObj | undefined {
+  if (!inputDict) return undefined;
+  if ("application/json" in inputDict) {
+    return inputDict["application/json"];
+  } else if ("*/*" in inputDict) {
+    return inputDict["*/*"];
+  } else if ("*/json" in inputDict) {
+    return inputDict["*/*"];
+  } else {
+    return undefined;
+  }
+}
+
+export function getHeader(
+  headers: { [key: string]: string } | Headers | null,
+  name: string,
+): string | null {
+  if (headers === null) return null;
+  if (typeof headers.get === "function") {
+    return headers.get(name) || headers.get(name.toLowerCase());
+  }
+  // @ts-ignore
+  return headers[name] || headers[name.toLowerCase()] || null;
+}
+
+export function getParam(
+  parameters: Record<string, any>,
+  key: string,
+  allowObjects: boolean = false,
+): any {
+  let out: any = undefined;
+  if (key in parameters) out = parameters[key];
+  if (!out) {
+    // Sometimes (rarely) the AI replaces hyphens with underscores. This is a hacky fix
+    const found = Object.keys(parameters).find(
+      (k) => k.replace(/-/g, "_") === key.replace(/-/g, "_"),
+    );
+    if (found) out = parameters[found];
+  }
+  if (out === undefined) return undefined;
+  if (allowObjects && typeof out === "object") {
+    return JSON.stringify(out);
+  }
+  return out;
+}
+
+export function parseErrorHtml(str: string): string {
+  const elements = [];
+  const regexes = [
+    /<title.*?>(.*)<\/title>/,
+    /<h1.*?>(.*)<\/h1>/,
+    /<h2.*?>(.*)<\/h2>/,
+    /<h3.*?>(.*)<\/h3>/,
+  ];
+  for (const regex of regexes) {
+    const match = str.match(regex);
+    if (match) {
+      elements.push(match[1].trim().replace(/\s+/g, " "));
+    }
+  }
+  const result = elements.filter(Boolean).join("\n");
+  return result.length > 0 ? result : str;
+}
