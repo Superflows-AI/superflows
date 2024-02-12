1c686f7b8c63e8c4bd3dbf80baf5db69080c8b2e
henry-pulver <henrypulver@hotmail.co.uk>
2024-02-12 17:07:38 +0000

    Add other cols to analytics code snippets table

--- supabase/migrations/20240111123041_bertie-ai-v2.sql
+++ supabase/migrations/20240111123041_bertie-ai-v2.sql
@@ -1,3 +1,6 @@
 alter table "public"."actions" add column "filtering_description" text not null default ''::text;
 alter table "public"."organizations" drop column "analytics_enabled";
 alter table "public"."organizations" add column "bertie_enabled" boolean not null default false;
+alter table "public"."analytics_code_snippets" add column "chosen_actions" text[];
+alter table "public"."analytics_code_snippets" add column "is_bertie" boolean not null default false;
+
