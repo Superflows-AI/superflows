8c6c8cdf35631d2379fb0f13a938c6c05020b491
henry-pulver <henrypulver@hotmail.co.uk>
2024-02-19 14:52:28 +0000

    TS gen tweaks - remove excess newlines & add tabs for indentation

--- lib/prompts/tsConversion.ts
+++ lib/prompts/tsConversion.ts
@@ -54,7 +54,9 @@ export function formatBodySchemaToTS(
         if (!isChoiceRequired(value, required.includes(key))) {
           return;
         }
-        paramString += `\n${key}${formatBodySchemaToTS(
+        paramString += `\n${"\t".repeat(
+          nestingLevel,
+        )}${key}${formatBodySchemaToTS(
           value,
           nestingLevel + 1,
           required.includes(key),
@@ -202,12 +204,12 @@ export function getActionTSSignature(
   const description = formatDescriptionTS(action.description).slice(4);
 
   const out = `${
-    description ? `\n/** ${description.split("\n").join("\n* ")} **/\n` : ""
+    description ? `/** ${description.split("\n").join("\n* ")} **/\n` : ""
   }${isAsync ? "async " : ""}function ${snakeToCamel(action.name)}(${
     paramString ? "args: {" + paramString + "\n}" : ""
   })`;
 
-  if (!includeReturnType) return out + "\n";
+  if (!includeReturnType) return out;
 
   // Add return type
   // @ts-ignore
@@ -244,7 +246,7 @@ export function getActionTSSignature(
   }
   // Set to any as fallback
   returnType = returnType || "any";
-  return out + ": " + returnType + "\n";
+  return out + ": " + returnType;
 }
 
 export function getActionTSSignatures(
