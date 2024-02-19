26f6a05fb1dc986e3299ac9e488bd091013d2379
henry-pulver <henrypulver@hotmail.co.uk>
2024-02-19 14:53:52 +0000

    Fix rare bug where action filtering output doesn't contain 'Thoughts'

--- lib/v2/prompts/actionFiltering.ts
+++ lib/v2/prompts/actionFiltering.ts
@@ -78,10 +78,9 @@ export function parseActionFilteringOutput(
     return { thoughts: "", selectedFunctions: [] };
   }
   return {
-    thoughts: output
-      .split("Selected functions:")[0]
-      .split("Thoughts:")[1]
-      .trim(),
+    thoughts:
+      output.split("Selected functions:")[0].split("Thoughts:")?.[1].trim() ??
+      "",
     selectedFunctions: output
       .split("Selected functions:")[1]
       .trim()
