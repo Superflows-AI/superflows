f18878753af59c817a5f76f0d9029065fe2690c6
henry-pulver <henrypulver@hotmail.co.uk>
2024-02-19 10:46:46 +0000

    Improve write action description prompt

--- lib/v2/prompts/writeActionDescription.ts
+++ lib/v2/prompts/writeActionDescription.ts
@@ -2,7 +2,12 @@ import { ChatGPTMessage } from "../../models";
 import { Action } from "../../types";
 import { getActionTSSignature } from "../../prompts/tsConversion";
 
-export function writeActionDescription(args: {
+const exampleDescriptionText = {
+  get: "Here's an example taken from a CRM of a good description for a function called search_companies:\n```\nSearch companies by name and can apply filters such as company id, industry, employees (> or <) and annual_revenue (> or <). Returns list of companies: each has an id, name, employee count and annual revenue\n```",
+  post: "Here's an example of a good description for a function called create_contact from a CRM:\n```\nCreate a contact at a company. Requires contact name, email and company_id. Phone number is optional\n```",
+};
+
+export function writeActionDescriptionPrompt(args: {
   action: Action;
   org: { name: string; description: string };
 }): ChatGPTMessage[] {
@@ -13,10 +18,11 @@ export function writeActionDescription(args: {
       } chatbot AI. Your task is write very short, concise descriptions for the functions. The descriptions you write will be used to select functions which are relevant to answer a user's request
 ${args.org.description ? "\n\n" + args.org.description : ""}
 
-Here's an example taken from a CRM of a good description for a function called search_companies:
-\`\`\`
-Search companies by name and can apply filters such as company id, industry, employees (> or <) and annual_revenue (> or <). Returns list of companies: each has an id, name, employee count and annual revenue
-\`\`\`
+${
+  args.action.request_method === "get"
+    ? exampleDescriptionText.get
+    : exampleDescriptionText.post
+}
 
 Here's the function to describe:
 \`\`\`
@@ -25,9 +31,9 @@ ${getActionTSSignature(args.action)}
 
 RULES:
 1. Make it extremely concise. Write in shorthand
-2. Group optional parameters together at the end of the list of parameters, as in the example
-3. DO NOT specify whether return parameters are optional, THIS IS NOT IMPORTANT
-4. If there are a large number of returned parameters, summarise them
+2. With parameters with general names (e.g. "id" or "name") specify what they refer to (Example: "company id")
+3. Mention if input parameters are optional, but DO NOT specify whether values in the return type are optional, THIS IS NOT IMPORTANT
+4. If there are many (>12) returned parameters, summarise them
 5. ONLY include the return type if the main purpose of the function is to retrieve data. DO NOT FORGET THIS`,
       role: "system",
     },
