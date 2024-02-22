ee3a1660354c8b49ab1e5433e74c098702ee69f6
henry-pulver <henrypulver@hotmail.co.uk>
2024-02-22 15:40:35 +0000

    Beef up parsers, prompts and test

--- __tests__/lib/v2/prompts/clarificationPrompt.test.ts
+++ __tests__/lib/v2/prompts/clarificationPrompt.test.ts
@@ -1,4 +1,5 @@
 import { parseClarificationOutput } from "../../../../lib/v2/prompts/clarificationPrompt";
+import tokenizer from "gpt-tokenizer";
 
 describe("parseOutput", () => {
   it("expected format: unclear", () => {
@@ -58,5 +59,29 @@ Tell user: Ask clarifying questions here. Be friendly (example: start with "Sure
       clear: true,
       tellUser: "",
     });
+  });
+  it("stream in real response", () => {
+    const response = `The is asking how to perform an action ("add a user rather than asking a question.
+2. From the FUNCTIONS provided, there is no FUNCTION called "addUser" or similar.
+3. However, there is a FUNCTION called "createContact" which could be used to add a user.
+
+Clear: False
+
+Tell user: Sure! It sounds like you want to add a new contact. To do that, we\'ll need some more details from you like the contact\'s name, email address, and which company they belong to. Once we have those details, I can use the createContact FUNCTION to add the new user for you. What other information can you provide?`;
+    const tokens = tokenizer.encode(response);
+    for (let i = 0; i < tokens.length; i++) {
+      const partial = tokenizer.decode(tokens.slice(0, i));
+      const parsed = parseClarificationOutput(partial);
+
+      expect(
+        "1. The user's request is to add a user, but there is no mention of users or any related data in the provided functions.\n2. The available functions are related to searching and updating companies, contacts, engagements, tasks and deals.\n3. As there is no function available to add a user, it is not possible to fulfill the user's request.",
+      ).toContain(parsed.thoughts);
+      if (parsed.tellUser) {
+        expect(parsed.clear).toBe(false);
+      }
+      expect(
+        "Sure! It sounds like you want to add a new contact. To do that, we'll need some more details from you like the contact's name, email address, and which company they belong to. Once we have those details, I can use the createContact FUNCTION to add the new user for you. What other information can you provide?",
+      ).toContain(parsed.tellUser);
+    }
   });
 });
