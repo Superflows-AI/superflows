1beed49f17abc3efe4ebd678c23782b5fd454378
henry-pulver <henrypulver@hotmail.co.uk>
2024-02-29 18:00:33 +0000

    Add routing tests

---
+++ __tests__/lib/v2/prompts/routing.test.ts
@@ -0,0 +1,36 @@
+import { parseRoutingOutput } from "../../../../lib/v2/prompts/routing";
+
+describe("parseRoutingOutput", () => {
+  it("should return null if the output does not match the expected format", () => {
+    expect(parseRoutingOutput("")).toBeNull();
+  });
+  it("should return the thoughts and choice if the output matches the expected format", () => {
+    expect(
+      parseRoutingOutput(
+        "Thoughts:\n1. Think step-by-step how to answer the user's request\n2. break down the user's request into steps\n3. specifically name EVERY SINGLE function and variable you will use\n4. consider if the user's request requires searching by name - use DIRECT if so\n5. compare the user's request with CRITERIA\n6. state your choice\n\nChoice: DIRECT\n",
+      ),
+    ).toEqual({
+      thoughts:
+        "1. Think step-by-step how to answer the user's request\n2. break down the user's request into steps\n3. specifically name EVERY SINGLE function and variable you will use\n4. consider if the user's request requires searching by name - use DIRECT if so\n5. compare the user's request with CRITERIA\n6. state your choice",
+      choice: "DIRECT",
+    });
+  });
+  it("not finished streaming", () => {
+    expect(
+      parseRoutingOutput(
+        "Thoughts:\n1. Think step-by-step how to answer the user's request\n2. break down the user's request into steps\n3. specifically name EVERY SINGLE function and variable you will use\n4. consider if the user's request requires searching by name - use DIRECT if so\n5. compare the user's request with CRITERIA\n6. state your choice\n\nChoice: DIREC",
+      ),
+    ).toBeNull();
+  });
+  it("not valid answer, but we move", () => {
+    expect(
+      parseRoutingOutput(
+        "Thoughts:\n1. Think step-by-step how to answer the user's request\n2. break down the user's request into steps\n3. specifically name EVERY SINGLE function and variable you will use\n4. consider if the user's request requires searching by name - use DIRECT if so\n5. compare the user's request with CRITERIA\n6. state your choice\n\nChoice: this ",
+      ),
+    ).toEqual({
+      thoughts:
+        "1. Think step-by-step how to answer the user's request\n2. break down the user's request into steps\n3. specifically name EVERY SINGLE function and variable you will use\n4. consider if the user's request requires searching by name - use DIRECT if so\n5. compare the user's request with CRITERIA\n6. state your choice",
+      choice: "this",
+    });
+  });
+});
