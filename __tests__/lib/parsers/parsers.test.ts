719252b1b5a6060fec3bc0e6090b5d5d0d577679
henry-pulver <henrypulver@hotmail.co.uk>
2024-03-15 12:13:59 +0000

    Add tests

--- __tests__/lib/parsers/parsers.test.ts
+++ __tests__/lib/parsers/parsers.test.ts
@@ -3,6 +3,7 @@ import {
   parseLegacyAnthropicStreamedData,
   parseFollowUpSuggestions,
   parseGPTStreamedData,
+  parseClaude3StreamedData,
 } from "../../../lib/parsers/parsers";
 
 describe("Parse GPT Streaming output", () => {
@@ -152,7 +153,7 @@ describe("parseFollowUpSuggestions", () => {
   });
 });
 
-describe("parseAnthropicStreamedData", () => {
+describe("parseLegacyAnthropicStreamedData", () => {
   it("anthropic example", () => {
     const testStr = `event: completion
 data: {"type":"completion","id":"compl_01XMs74TyRye1whxj41pJrQh","completion":" The","stop_reason":null,"model":"claude-instant-1.2","stop":null,"log_id":"compl_01XMs74TyRye1whxj41pJrQh"}
@@ -217,6 +218,81 @@ data: {"t`;
       completeChunks: [],
       done: false,
       incompleteChunk: 'completion\ndata: {"t',
+    });
+  });
+});
+
+describe("parseClaude3StreamedData", () => {
+  it("claude message start", () => {
+    const testStr = `event: message_start
+data: {"type": "message_start", "message": {"id": "msg_1nZdL29xx5MUA1yADyHTEsnR8uuvGzszyY", "type": "message", "role": "assistant", "content": [], "model": "claude-3-opus-20240229", "stop_reason": null, "stop_sequence": null, "usage": {"input_tokens": 25, "output_tokens": 1}}}
+`;
+    const out = parseClaude3StreamedData(testStr);
+    expect(out).toStrictEqual({
+      completeChunks: [],
+      done: false,
+      incompleteChunk: null,
+    });
+  });
+  it("claude message start with text", () => {
+    const testStr = `event: message_start
+data: {"type": "message_start", "message": {"id": "msg_1nZdL29xx5MUA1yADyHTEsnR8uuvGzszyY", "type": "message", "role": "assistant", "content": ["Hello"], "model": "claude-3-opus-20240229", "stop_reason": null, "stop_sequence": null, "usage": {"input_tokens": 25, "output_tokens": 1}}}
+`;
+    const out = parseClaude3StreamedData(testStr);
+    expect(out).toStrictEqual({
+      completeChunks: ["Hello"],
+      done: false,
+      incompleteChunk: null,
+    });
+  });
+  it("contains ping", () => {
+    const testStr = `event: content_block_start
+data: {"type": "content_block_start", "index":0, "content_block": {"type": "text", "text": ""}}
+
+event: ping
+data: {"type": "ping"}
+
+event: content_block_delta
+data: {"type": "content_block_delta", "index": 0, "delta": {"type": "text_delta", "text": "Hello"}}
+`;
+    const out = parseClaude3StreamedData(testStr);
+    expect(out).toStrictEqual({
+      completeChunks: ["", "Hello"],
+      done: false,
+      incompleteChunk: null,
+    });
+  });
+  it("incomplete chunk", () => {
+    const testStr = `event: content_block_delta
+data: {"type": "content_block_delta", "index": 0, "delta": {"type": "text_delta", "text": "Hello"}}
+
+event: content_block_delta
+data: {"type": "content_block_delta", "index": 0, "delta": {"type": "text`;
+    const out = parseClaude3StreamedData(testStr);
+    expect(out).toStrictEqual({
+      completeChunks: ["Hello"],
+      done: false,
+      incompleteChunk: `content_block_delta
+data: {"type": "content_block_delta", "index": 0, "delta": {"type": "text`,
+    });
+  });
+  it("When the fun stops; stop", () => {
+    const testStr = `event: content_block_delta
+data: {"type": "content_block_delta", "index": 0, "delta": {"type": "text_delta", "text": "!"}}
+
+event: content_block_stop
+data: {"type": "content_block_stop", "index": 0}
+
+event: message_delta
+data: {"type": "message_delta", "delta": {"stop_reason": "end_turn", "stop_sequence":null, "usage":{"output_tokens": 15}}}
+
+event: message_stop
+data: {"type": "message_stop"}`;
+    const out = parseClaude3StreamedData(testStr);
+    expect(out).toStrictEqual({
+      completeChunks: ["!"],
+      done: true,
+      incompleteChunk: null,
     });
   });
 });
