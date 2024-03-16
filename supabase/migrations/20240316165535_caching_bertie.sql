d12b5d1c9c73c1cdcf65ca2714490f7f8765ed7a
henry-pulver <henrypulver@hotmail.co.uk>
2024-03-16 17:01:00 +0000

    Add constraint to chat_messages

---
+++ supabase/migrations/20240316165535_caching_bertie.sql
@@ -0,0 +1,9 @@
+-- Store conversation history summary
+ALTER table "public.chat_messages" add column "chat_summary" text not null default ''::text;
+
+-- Store filtering results
+ALTER table "public.chat_messages" add column "chosen_actions" text[];
+
+-- Store routing result
+ALTER table "public.chat_messages" add column "chosen_route" text;
+
