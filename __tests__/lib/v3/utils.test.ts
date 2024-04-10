5f99fbbf91ff6e0c064d77eaa48d28dc84c9ebd0
henry-pulver <henrypulver@hotmail.co.uk>
2024-04-10 17:41:06 +0100

    Update iso to human readable functions to new date format

--- __tests__/lib/v3/utils.test.ts
+++ __tests__/lib/v3/utils.test.ts
@@ -1,24 +1,8 @@
-import { ISOMonthsToReadable } from "../../../lib/v3/utils";
+import { convertIsoToHumanReadable } from "../../../lib/v3/utils";
 
-describe("ISOMonthsToReadable", () => {
+describe("convertIsoToHumanReadable", () => {
   it("basic", () => {
-    expect(ISOMonthsToReadable(["2024-01", "2024-02"])).toEqual(
-      "January and February 2024",
-    );
-    expect(
-      ISOMonthsToReadable([
-        "2024-01",
-        "2024-02",
-        "2024-03",
-        "2024-04",
-        "2024-05",
-      ]),
-    ).toEqual("January, February, March, April and May 2024");
-    expect(ISOMonthsToReadable(["2025-01", "2024-12"])).toEqual(
-      "December 2024 and January 2025",
-    );
-    expect(ISOMonthsToReadable(["2024-01", "2025-02", "2025-03"])).toEqual(
-      "January 2024, February and March 2025",
-    );
+    expect(convertIsoToHumanReadable("2024-01-01")).toEqual("1st January 2024");
+    expect(convertIsoToHumanReadable("2024-05-30")).toEqual("30th May 2024");
   });
 });
