ae2366b66a7fe8ad2916228cd523954097022ef1
henry-pulver <henrypulver@hotmail.co.uk>
2024-04-18 16:16:10 +0100

    Add ISO to human readable to code execution

--- supabase/functions/execute-code-2/index.ts
+++ supabase/functions/execute-code-2/index.ts
@@ -290,3 +290,37 @@ export function isValidBody<T extends Record<string, unknown>>(
   }
   return safeParseOut.success;
 }
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export function convertIsoToHumanReadable(dateStr: string): string {
+  let date = new Date(dateStr);
+
+  let day = date.getDate();
+  let monthIndex = date.getMonth();
+  let year = date.getFullYear();
+
+  let suffix = "th";
+  if ([1, 21, 31].includes(day)) {
+    suffix = "st";
+  } else if ([2, 22].includes(day)) {
+    suffix = "nd";
+  } else if ([3, 23].includes(day)) {
+    suffix = "rd";
+  }
+
+  return `${day}${suffix} ${monthNames[monthIndex]} ${year}`;
+}
