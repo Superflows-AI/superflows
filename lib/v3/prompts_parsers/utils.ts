c5acbcd10b56aa82c611defb997df560b95a0b4b
henry-pulver <henrypulver@hotmail.co.uk>
2024-04-18 17:35:09 +0100

    Minor tweaks that should have been in Cassius merge

--- lib/v3/prompts_parsers/utils.ts
+++ lib/v3/prompts_parsers/utils.ts
@@ -1,18 +1,5 @@
 import { ApprovalVariable } from "../../types";
 
-export function convertToOpus(prompt: string): string {
-  return prompt
-    .split("\n\n")
-    .map((text) => {
-      const numberedList = text.match(/^(\w+):\n((\d\.\s*.+)+)/g);
-      if (!numberedList) return text;
-      return `<${numberedList[1].toLowerCase()}>
-${numberedList[2]}
-</${numberedList[1].toLowerCase()}>`;
-    })
-    .join("\n\n");
-}
-
 export function variableToTypeDef(variable: ApprovalVariable): string {
   return `type ${variable.typeName} = ${variable.type}${
     variable.description ? ` // ${variable.description}` : ""
