b67642d7055ccb736ae8c3ed20601885093623c0
henry-pulver <henrypulver@hotmail.co.uk>
2024-05-20 18:06:55 +0100

    Make fnNames actually unique in the DB

---
+++ supabase/migrations/20240520165609_unique-fnNames.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION unique_fnname_per_org()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW."fnName" != '' AND EXISTS (
+    SELECT 1 FROM "public"."approval_answers"
+    WHERE "org_id" = NEW."org_id" AND "fnName" = NEW."fnName"
+  ) THEN
+    RAISE EXCEPTION 'Duplicate org_id and fnName combination';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_unique_fnname_per_org
+BEFORE INSERT OR UPDATE ON "public"."approval_answers"
+FOR EACH ROW EXECUTE FUNCTION unique_fnname_per_org();
