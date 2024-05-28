149005b3af9c7f5cdb320ed3ff5cb6dcd9b9abee
henry-pulver <henrypulver@hotmail.co.uk>
2024-05-28 18:14:05 +0100

    No need to double-translate with Espanol in chat-to-docs

--- lib/v3/prompts_parsers/chatToDocs.ts
+++ lib/v3/prompts_parsers/chatToDocs.ts
@@ -29,21 +29,29 @@ ${facts.map((fact, i) => `${i + 1}. ${fact}`).join("\n")}
 2. ONLY include information from the documentation that answers the question. DO NOT include irrelevant information
 3. DO NOT tell the user to find the answer in the documentation
 4. Tell the user if the docs do not cover a topic
-5. Write <tellUser></tellUser> in ${
-        language ?? "the same language as the user"
-      }. You may have to translate the documentation to do this 
+5. Write in ${language ?? "the same language as the user"}
 6. BE CONCISE
 7. Output your answer following <format></format>
 </rules>
 
 <format>
 <thinking>
-1. Think step-by-step: What language should I write <tellUser></tellUser> in?
+${
+  language !== "Spanish"
+    ? `1. Think step-by-step: What language should I write in?
 2. What are the most relevant pieces of information in the docs?
-3. Do the docs answer the question?
+3. Do the docs answer the question?`
+    : `1. Piensa paso a paso: ¿En qué idioma debo escribir?
+2. ¿Cuáles son los datos más relevantes de los documentos?
+3. ¿Responden los médicos a la pregunta?`
+}
 </thinking>
 <tellUser>
-Answer the user's question if the docs contain the answer.
+${
+  language !== "Spanish"
+    ? "Answer the user's question if the docs contain the answer."
+    : "Responda la pregunta del usuario si los documentos contienen la respuesta."
+}
 </tellUser>
 </format>`,
     },
@@ -53,13 +61,20 @@ Answer the user's question if the docs contain the answer.
     },
     {
       role: "assistant",
-      content: `<thinking>
-1. What language should I write <tellUser></tellUser> in?
+      content:
+        language !== "Spanish"
+          ? `<thinking>
+1. What language should I write in?
   - ${
     language ? `I must write in ${language}` : "The same language as the user"
   }
 2. What are the most relevant pieces of information in the docs?
-  - The most relevant piece of information is`,
+  - The most relevant piece of information is`
+          : `<thinking>
+1. ¿En qué idioma debo escribir?
+  - debo escribir en español
+2. ¿Cuáles son los datos más relevantes de los documentos?
+  - La información más relevante es`,
     },
   ];
 }
