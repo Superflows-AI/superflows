0d1e325f65095425a9c5be914a049fc175093742
henry-pulver <henrypulver@hotmail.co.uk>
2024-05-28 13:58:26 +0100

    Remove routing messages from approval

--- lib/v3/edge-runtime/ai.ts
+++ lib/v3/edge-runtime/ai.ts
@@ -165,305 +165,187 @@ export async function Cassius(
       generatedMessages.push(userRequest);
     }
 
-    // If DOCS and CODE both possible, generate the routing output if not present
-    let routingMsg = pastMessages.find((m) => m.message_type === "routing");
-    // If no chat-to-docs, always route to code
-    let route: "CODE" | "DOCS" | "" = !org.chat_to_docs_enabled
-      ? "CODE"
-      : // If no actions, always to chat-to-docs
-      actions.length === 0
-      ? "DOCS"
-      : // If existing routing message, use that
-      routingMsg
-      ? parseRoutingOutputv3(routingMsg.raw_text)?.choice ?? ""
-      : "";
-    console.log("Route", route);
-
-    // If there are variables, you know it's a code question (variables don't work with docs)
-    if (!route && userRequest.raw_text !== "{}") {
-      routingMsg = await addNewMessageToDB(
-        `<thinking>\nThe question contains parameters, so requires code.\n</thinking>\n<choice>CODE</choice>`,
-        "routing",
-      );
-      generatedMessages.push(routingMsg);
-      route = "CODE";
-      streamInfo(routingMsg);
-    } else if (!route) {
-      console.log("No route found, generating one...");
-      const prompt = routingPromptv3({
-        userRequest: getUserMessageText(
+    let filteringMessage = pastMessages.find(
+      (m) => m.message_type === "filtering",
+    );
+    console.log("Filtering message", filteringMessage);
+    if (!filteringMessage) {
+      console.log("No filtering message found, generating one...");
+      const filterOutput = await filterActions(
+        getUserMessageText(
           primaryQuestion.text,
           JSON.parse(userRequest.raw_text),
         ),
-        org: org,
-        actions: actions,
+        actions,
+        org,
+        "ft:gpt-3.5-turbo-0613:superflows:general-2:81WtjDqY",
+      );
+      filteringMessage = await addNewMessageToDB(
+        `<thinking>\n${filterOutput.thoughts.join(
+          "\n",
+        )}\n</thinking><selected_functions>\n${filterOutput.actions
+          .map((a) => snakeToCamel(a.name))
+          .join("\n")}\n</selected_functions>`,
+        "filtering",
+      );
+      streamInfo(filteringMessage); // Send the user's request to the frontend
+      generatedMessages.push(filteringMessage);
+    }
+    const filteredActionNames = parseFilteringOutputv3(
+      filteringMessage.raw_text,
+      actions.map((a) => snakeToCamel(a.name)),
+    ).selectedFunctions;
+    console.log("filteredActionNames", filteredActionNames);
+    const filteredActions = actions.filter((a) =>
+      filteredActionNames.includes(snakeToCamel(a.name)),
+    );
+    console.log(
+      "Filtered actions",
+      filteredActions.map((a) => a.name),
+    );
+    if (filteredActions.length === 0) {
+      console.error("No actions selected");
+      void streamInfo({
+        role: "error",
+        content: "No actions selected",
       });
-      const routingLlmOut = `<thinking>\n1.${await exponentialRetryWrapper(
-        getLLMResponse,
-        [prompt, routingLLMParams, CASSIUS_MODEL],
-        3,
-      )}</choice>`;
-      routingMsg = await addNewMessageToDB(routingLlmOut, "routing");
-      streamInfo(routingMsg); // Send the user's request to the frontend
-      generatedMessages.push(routingMsg);
-
-      const parsedRoute = parseRoutingOutputv3(routingLlmOut)?.choice;
-      if (!parsedRoute) {
-        console.error("Failed to parse routing output");
-        return false;
-      }
-      route = parsedRoute;
+      await addNewMessageToDB("No actions selected", "error");
+      return false;
     }
 
-    if (route === "DOCS") {
-      console.log("Routing to DOCS");
-      const questionMessage = {
-        role: "user",
-        content: primaryQuestion.text,
-      } as ChatGPTMessage;
-      const hallucinatedRes = await exponentialRetryWrapper(
-        getLLMResponse,
-        [
-          [hallucinateDocsSystemPrompt("", org), questionMessage],
-          { max_tokens: 600, temperature: 0.6 },
-          "gpt-3.5-turbo-0125",
-        ],
-        3,
-      );
-      console.log("Hallucination: ", hallucinatedRes);
-
-      // We do embedding and similarity search on the hallucinated docs
-      const relevantDocs = await getRelevantDocChunks(
-        hallucinatedRes,
-        org.id,
-        3,
-        supabase,
-      );
-
-      let docSearchGPTMessage = {
-        role: "function",
-        content: relevantDocs.text,
-        name: "search_docs",
-        urls: relevantDocs.urls,
-      } as Extract<GPTMessageInclSummary, { role: "function" }>;
-
-      const docSearchMessage = await addNewMessageToDB(
-        hallucinatedRes,
-        "function",
-        [docSearchGPTMessage],
-      );
-      generatedMessages.push(docSearchMessage);
-
-      const { cleanedMessages, originalToPlaceholderMap } = sanitizeMessages(
-        [questionMessage, docSearchGPTMessage],
-        true,
-      );
-
-      let prompt: ChatGPTMessage[] = [
-        chatToDocsPrompt(
-          "",
-          org,
-          Object.entries(originalToPlaceholderMap).length > 0,
-          null,
-        ),
-        ...cleanedMessages.map(MessageInclSummaryToGPT),
-      ];
-
+    // Generate the code
+    let codeGenerated = false;
+    let codeMessage = pastMessages.find((m) => m.message_type === "code");
+    if (!codeMessage) {
+      console.log("No code message found, generating one...");
+      const prompt = getCodeGenPromptv3({
+        question: primaryQuestion.text,
+        org: org,
+        filteredActions,
+        variables: approvalVariableData,
+      });
       logPrompt(prompt);
-      const res = await exponentialRetryWrapper(
+      const codeLlmOut = `<plan>\n1. The most helpful answer would include:\n  -${await exponentialRetryWrapper(
         getLLMResponse,
-        [prompt, { max_tokens: 600, temperature: 0.2 }, "gpt-3.5-turbo-0125"],
+        [prompt, codeGenLLMParams, CASSIUS_MODEL],
         3,
+      )}\n</code>`;
+      codeMessage = await addNewMessageToDB(codeLlmOut, "code");
+      generatedMessages.push(codeMessage);
+      codeGenerated = true;
+    }
+
+    // Run the code
+    if (codeMessage?.generated_output.length === 0) {
+      console.log("Generating code output");
+      const parsedCode = parseCodeGenv3(codeMessage!.raw_text);
+      const userSetValues: Record<string, any> = JSON.parse(
+        userRequest?.raw_text ?? "{}",
       );
-      const docsMessage = await addNewMessageToDB(res, "text");
-      streamInfo(docsMessage);
-      generatedMessages.push(docsMessage);
-    } else {
-      let filteringMessage = pastMessages.find(
-        (m) => m.message_type === "filtering",
-      );
-      console.log("Filtering message", filteringMessage);
-      if (!filteringMessage) {
-        console.log("No filtering message found, generating one...");
-        const filterOutput = await filterActions(
-          getUserMessageText(
-            primaryQuestion.text,
-            JSON.parse(userRequest.raw_text),
-          ),
-          actions,
+      const res = await supabase.functions.invoke("execute-code-2", {
+        body: JSON.stringify({
+          actionsPlusApi: filteredActions,
           org,
-          "ft:gpt-3.5-turbo-0613:superflows:general-2:81WtjDqY",
+          code: convertWrittenCodeToExecutable(
+            parsedCode.code,
+            approvalVariableData.map((v) => ({
+              ...v,
+              default: userSetValues[v.name] ?? v.default,
+            })),
+          ),
+          userApiKey: reqData.user_api_key,
+        }),
+      });
+      if (res.error) {
+        let errorMessage = "";
+        if (res.error instanceof FunctionsHttpError) {
+          errorMessage = await res.error.context.text();
+          if (errorMessage === "Internal Server Error") {
+            errorMessage =
+              "Timed out. Caused either by too much CPU time taken, or too long waiting for APIs.";
+          }
+          console.log("Function returned an error", errorMessage);
+        } else if (res.error instanceof FunctionsRelayError) {
+          errorMessage = res.error.message;
+          console.log("Relay error:", res.error.message);
+        } else if (res.error instanceof FunctionsFetchError) {
+          errorMessage = res.error.message;
+          console.log("Fetch error:", res.error.message);
+        }
+        await addNewMessageToDB(
+          "Failed to execute code: " + errorMessage,
+          "error",
         );
-        filteringMessage = await addNewMessageToDB(
-          `<thinking>\n${filterOutput.thoughts.join(
-            "\n",
-          )}\n</thinking><selected_functions>\n${filterOutput.actions
-            .map((a) => snakeToCamel(a.name))
-            .join("\n")}\n</selected_functions>`,
-          "filtering",
-        );
-        streamInfo(filteringMessage); // Send the user's request to the frontend
-        generatedMessages.push(filteringMessage);
-      }
-      const filteredActionNames = parseFilteringOutputv3(
-        filteringMessage.raw_text,
-        actions.map((a) => snakeToCamel(a.name)),
-      ).selectedFunctions;
-      console.log("filteredActionNames", filteredActionNames);
-      const filteredActions = actions.filter((a) =>
-        filteredActionNames.includes(snakeToCamel(a.name)),
-      );
-      console.log(
-        "Filtered actions",
-        filteredActions.map((a) => a.name),
-      );
-      if (filteredActions.length === 0) {
-        console.error("No actions selected");
-        void streamInfo({
+        streamInfo({
           role: "error",
-          content: "No actions selected",
+          content: "Failed to execute code" + res.error.toString(),
         });
-        await addNewMessageToDB("No actions selected", "error");
+        console.error("Failed to execute code", res.error.toString());
         return false;
       }
-
-      // Generate the code
-      let codeGenerated = false;
-      let codeMessage = pastMessages.find((m) => m.message_type === "code");
-      if (!codeMessage) {
-        console.log("No code message found, generating one...");
-        const prompt = getCodeGenPromptv3({
-          question: primaryQuestion.text,
-          org: org,
-          filteredActions,
-          variables: approvalVariableData,
-        });
-        logPrompt(prompt);
-        const codeLlmOut = `<plan>\n1. The most helpful answer would include:\n  -${await exponentialRetryWrapper(
-          getLLMResponse,
-          [prompt, codeGenLLMParams, CASSIUS_MODEL],
-          3,
-        )}\n</code>`;
-        codeMessage = await addNewMessageToDB(codeLlmOut, "code");
-        generatedMessages.push(codeMessage);
-        codeGenerated = true;
-      }
-
-      // Run the code
-      if (codeMessage?.generated_output.length === 0) {
-        console.log("Generating code output");
-        const parsedCode = parseCodeGenv3(codeMessage!.raw_text);
-        const userSetValues: Record<string, any> = JSON.parse(
-          userRequest?.raw_text ?? "{}",
-        );
-        const res = await supabase.functions.invoke("execute-code-2", {
-          body: JSON.stringify({
-            actionsPlusApi: filteredActions,
-            org,
-            code: convertWrittenCodeToExecutable(
-              parsedCode.code,
-              approvalVariableData.map((v) => ({
-                ...v,
-                default: userSetValues[v.name] ?? v.default,
-              })),
-            ),
-            userApiKey: reqData.user_api_key,
-          }),
-        });
-        if (res.error) {
-          let errorMessage = "";
-          if (res.error instanceof FunctionsHttpError) {
-            errorMessage = await res.error.context.text();
-            if (errorMessage === "Internal Server Error") {
-              errorMessage =
-                "Timed out. Caused either by too much CPU time taken, or too long waiting for APIs.";
-            }
-            console.log("Function returned an error", errorMessage);
-          } else if (res.error instanceof FunctionsRelayError) {
-            errorMessage = res.error.message;
-            console.log("Relay error:", res.error.message);
-          } else if (res.error instanceof FunctionsFetchError) {
-            errorMessage = res.error.message;
-            console.log("Fetch error:", res.error.message);
-          }
-          await addNewMessageToDB(
-            "Failed to execute code: " + errorMessage,
-            "error",
-          );
-          streamInfo({
-            role: "error",
-            content: "Failed to execute code" + res.error.toString(),
-          });
-          console.error("Failed to execute code", res.error.toString());
-          return false;
-        }
-        const returnedData = res.data as ExecuteCode2Item[] | null;
-        console.log("Returned data", returnedData);
-        const codeOk = checkCodeExecutionOutput(returnedData);
-        if (!codeOk.isValid) {
-          console.warn("Error from generated code:", codeOk.error);
-          // Retry code generation from scratch once if not 4XX & code generated
-          if (
-            shouldRetry &&
-            codeGenerated &&
-            !codeOk.error.includes('"status": 4')
-          ) {
-            const { error: delCodeError } = await supabase
-              .from("approval_answer_messages")
-              .delete()
-              .eq("id", codeMessage.id);
-            if (delCodeError) throw new Error(delCodeError.message);
-            // Dirty, but retries and then returns
-            return Cassius(
-              controller,
-              reqData,
-              actions,
-              org,
-              approvalAnswersData,
-              approvalVariableData,
-              false,
-            );
-          }
-        }
-        if (returnedData !== null) {
-          const graphDataArr = convertToGraphData(returnedData);
-          const generated_output = graphDataArr.map((m) => ({
-            role: m.role === "graph" ? "function" : m.role,
-            name: m.role === "graph" ? "plot" : "logs",
-            content:
-              typeof m.content === "string"
-                ? m.content
-                : JSON.stringify(m.content),
-          }));
-          if (!codeOk.isValid) {
-            generated_output.push({
-              role: "error",
-              name: "error", // For TS reasons
-              content: codeOk.error,
-            });
-          }
-          codeMessage.generated_output = generated_output;
-          const { error: insertCodeMessageErr } = await supabase
+      const returnedData = res.data as ExecuteCode2Item[] | null;
+      console.log("Returned data", returnedData);
+      const codeOk = checkCodeExecutionOutput(returnedData);
+      if (!codeOk.isValid) {
+        console.warn("Error from generated code:", codeOk.error);
+        // Retry code generation from scratch once if not 4XX & code generated
+        if (
+          shouldRetry &&
+          codeGenerated &&
+          !codeOk.error.includes('"status": 4')
+        ) {
+          const { error: delCodeError } = await supabase
             .from("approval_answer_messages")
-            .update({ generated_output })
+            .delete()
             .eq("id", codeMessage.id);
-          if (insertCodeMessageErr) {
-            console.error(
-              "Failed to insert code message",
-              insertCodeMessageErr,
-            );
-            return false;
-          }
-          streamInfo({ ...codeMessage, generated_output }); // Send the user's request to the frontend
-          if (!codeOk.isValid) return false;
-        } else {
-          console.error("No output data from generated code");
-          void streamInfo({
+          if (delCodeError) throw new Error(delCodeError.message);
+          // Dirty, but retries and then returns
+          return Cassius(
+            controller,
+            reqData,
+            actions,
+            org,
+            approvalAnswersData,
+            approvalVariableData,
+            false,
+          );
+        }
+      }
+      if (returnedData !== null) {
+        const graphDataArr = convertToGraphData(returnedData);
+        const generated_output = graphDataArr.map((m) => ({
+          role: m.role === "graph" ? "function" : m.role,
+          name: m.role === "graph" ? "plot" : "logs",
+          content:
+            typeof m.content === "string"
+              ? m.content
+              : JSON.stringify(m.content),
+        }));
+        if (!codeOk.isValid) {
+          generated_output.push({
             role: "error",
-            content: "No output data from generated code",
+            name: "error", // For TS reasons
+            content: codeOk.error,
           });
+        }
+        codeMessage.generated_output = generated_output;
+        const { error: insertCodeMessageErr } = await supabase
+          .from("approval_answer_messages")
+          .update({ generated_output })
+          .eq("id", codeMessage.id);
+        if (insertCodeMessageErr) {
+          console.error("Failed to insert code message", insertCodeMessageErr);
           return false;
         }
+        streamInfo({ ...codeMessage, generated_output }); // Send the user's request to the frontend
+        if (!codeOk.isValid) return false;
+      } else {
+        console.error("No output data from generated code");
+        void streamInfo({
+          role: "error",
+          content: "No output data from generated code",
+        });
+        return false;
       }
 
       // Explanation message
