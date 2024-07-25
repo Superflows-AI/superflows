100499f69ee8edac95562de31e5f1cb661b9167d
cnbhavesh <bhavesh@capitalnumbers.com>
2024-07-25 18:58:07 +0530

    Update index.ts
    Commented auth

--- supabase/functions/execute-code/index.ts
+++ supabase/functions/execute-code/index.ts
@@ -32,12 +32,12 @@ const headers = { "content-type": "application/json" };
 Deno.serve(async (req) => {
   const authHeader = req.headers.get("Authorization");
   // Auth check
-  if (authHeader !== "Bearer " + Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")) {
-    return new Response(JSON.stringify({ error: "Invalid auth" }), {
-      status: 401,
-      headers,
-    });
-  }
+  // if (authHeader !== "Bearer " + Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")) {
+  //   return new Response(JSON.stringify({ error: "Invalid auth" }), {
+  //     status: 401,
+  //     headers,
+  //   });
+  // }
 
   // Check that the user hasn't surpassed the rate limit
   if (ratelimit) {
